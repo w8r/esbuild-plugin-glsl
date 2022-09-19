712606f1d36f13ffbe45fdf36835fa50037a97e9
Alexander Milevski <alexander@linkurio.us>
2022-09-19 22:06:02 +0200

    Commited built code to use until the PR is merged

---
+++ dist/types/index.d.ts
@@ -0,0 +1,16 @@
+import { Plugin } from "esbuild";
+/**
+ * GLSL plugin options.
+ */
+export interface GLSLOptions {
+    minify?: boolean;
+    mangle?: boolean;
+}
+/**
+ * An options wrapper function that returns the GLSL plugin.
+ *
+ * @param options The options.
+ * @return The plugin.
+ */
+declare function glsl({ minify, mangle }?: GLSLOptions): Plugin;
+export { glsl, glsl as default };
