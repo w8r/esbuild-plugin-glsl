d7016b96c49ff1e7732a9bb5fab6c10b6b682c8a
Alexander Milevski <alexander@linkurio.us>
2022-09-20 14:57:15 +0200

    Cleanup before the release

--- dist/types/mangle.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function makeNewIdentifier(): string;
-/**
- * Checks all words in a string and mangles the ids
- * @param source
- * @returns mangled code
- */
-export declare const mangle: (source: string) => string;
--- dist/types/minifyShader.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Minifies the given GLSL source code.
- *
- * Based on https://github.com/vwochnik/rollup-plugin-glsl
- *
- * @param source The source code.
- * @return The minified code.
- */
-export declare function minifyShader(source: string, mangle: boolean): string;
